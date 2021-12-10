45aea23608d5a5bcd95b045d9b9142dfe9d6f73f
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2021-12-10 09:53:34 +0530

    completed 4 modules of course

---
+++ course/src/ApprovalCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import React from "react";
+
+const ApprovalCard = () => {
+  return (
+    <div className="ui card">
+      <div className="content">Are you Sure ?
+      
+      </div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="red">
+            
+            </div> 
+              <div className="Green">
+              
+              </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApprovalCard;
--- course/src/index.js
+++ course/src/index.js
@@ -1,14 +1,50 @@
 //Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail.js';
+
 
 //Create a react component
-Const App = () => {
+const App = () => {
+    
+   /*  function getTime()
+    {
+        //let date = new Date(Date.UTC());
+        return new Date().toLocaleString()
+    } */
+
+//    const buttonText={joe:"hi"};
+
      return(
-         <div>
-             <label class="label" for="name">Enter Name:</label>
-             <input id="name" type="text" />>
-             <button style={{"backgroundColor:blue; color:white; "}}></button>
+         <div className="ui container comments">
+            <CommentDetail name="e" timeAgo={"28/2 6:00 PM"} comment="a" image={faker.image.avatar()}/>
+            <CommentDetail name="d" timeAgo={"28/2 6:00 PM"} comment="b" image={faker.image.avatar()}/>
+            <CommentDetail name="c" timeAgo={"28/2 6:00 PM"} comment="c" image={faker.image.avatar()}/>
+            <CommentDetail name="b" timeAgo={"28/2 6:00 PM"} comment="d" image={faker.image.avatar()}/>
+            <CommentDetail name="a" timeAgo={"28/2 6:00 PM"} comment="e" image={faker.image.avatar()}/>
+            {/*  <div className="comment">
+                 <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                 </a>
+
+                 <div className="content">
+                     <a href="/" className="author">
+                        Sam
+                     </a>
+                 
+                 <div className="metadata">
+                     <span className="date">
+                         Today at 6:00PM
+                     </span>
+                 </div>
+                 </div>
+                 <div className="text">Nice blog post! </div>
+             </div> */}
+             {/* <label className="label" htmlFor="name">Enter Name:</label>
+             <input id="name" type="text"/>
+             <button style={{ backgroundColor:'blue',color:'white' }}>{getTime()}</button> */}
+             
         </div>
      )
 }
